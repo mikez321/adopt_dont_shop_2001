17de9a3375fd0d9eb385a5db0d6d1b039154cb02
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-19 14:33:43 -0600

    Begin making a test for pets

---
+++ spec/features/pets/pets_have_an_index_spec.rb
@@ -0,0 +1,21 @@
+# require 'rails_helper'
+#
+# RSpec.describe "when a visitor visits pets" do
+#   describe "they see each pet in the system" do
+#     it "Including their image, name age, sex and shelter" do
+#
+#       pet1 = Pet.create(name: "Charlotte",
+#                         age: "13",
+#                         sex: "Female",
+#                       )
+#
+#       visit '/pets'
+#
+#       expect(page).to have_content(pet1.name)
+#       expect(page).to have_content(pet1.age)
+#       expect(page).to have_content(pet1.sex)
+#       expect(page).to have_content(pet1.shelter)
+#
+#     end
+#   end
+# end
