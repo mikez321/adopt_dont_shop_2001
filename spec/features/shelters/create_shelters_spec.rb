30c1fc36822af0a0f2dd56bad92bf42cacdd07d8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-18 19:29:05 -0600

    Update test for add shelter

--- spec/features/shelters/create_shelters_spec.rb
+++ spec/features/shelters/create_shelters_spec.rb
@@ -10,11 +10,18 @@ RSpec.describe 'new shelter page' do
 
       expect(current_path).to eq('/shelters/new')
 
-      expect(page).to have_content('Name:')
-      expect(page).to have_content('Address:')
-      expect(page).to have_content('City:')
-      expect(page).to have_content('State:')
-      expect(page).to have_content('Zip:')
+      fill_in 'name', with: "The Dog Rescuerer"
+      fill_in 'address', with: "1234 Floor"
+      fill_in 'city', with: "Denver"
+      fill_in 'state', with: "CO"
+      fill_in 'zip', with: "80232"
+      click_on 'Submit'
+
+      expect(current_path).to eq("/")
+      
+      expect(page).to have_content('The Dog Rescuerer')
+
+
 
     end
   end
