9117caee8592876d5042c0925bf489d935e7b2eb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-18 23:32:06 -0600

    Update root to be /shelters instead of /

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  root 'shelters#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'shelters#index'
+  get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
   post '/shelters/', to: 'shelters#create'
