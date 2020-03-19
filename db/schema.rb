6de0024b8673ea2b8ba2aa8c20ede04680c665af
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-19 16:55:51 -0600

    Removed shelterid from local db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319205545) do
+ActiveRecord::Schema.define(version: 20200319225505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20200319205545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "sex"
-    t.integer "shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
