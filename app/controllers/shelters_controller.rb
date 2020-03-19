3d9c670cf4514d9e20afc3ffcd508aa12501e88b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-18 22:02:25 -0600

    Create edit destroy and update methods

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -16,6 +16,28 @@ class SheltersController < ApplicationController
     redirect_to '/'
   end
 
+  def destroy
+    Shelter.destroy(params[:id])
+    redirect_to '/'
+  end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+        name: params[:shelter][:name],
+        address: params[:shelter][:address],
+        city: params[:shelter][:city],
+        state: params[:shelter][:state],
+        zip: params[:shelter][:zip]
+      })
+
+      redirect_to "/shelters/#{shelter.id}/"
+  end
+
   private
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
