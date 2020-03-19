9240fb955831fbfd722e216f2f39f1dfff3741f9
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-18 21:55:24 -0600

    Create test to delete shelter

---
+++ spec/features/shelters/delete_a_shelter_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor when I visit a shelter page" do
+  describe "And I click the delete button" do
+    it "i can delete the shelter" do
+
+      animal_shelter = Shelter.create(name: "Joe Exotic's House",
+                            address: "7785 Jay St",
+                            city: "Lakewood",
+                            state: "CO",
+                            zip: "80232")
+
+      visit "/shelters/#{animal_shelter.id}"
+
+      click_button 'Delete'
+
+      expect(current_path).to eq("/")
+      expect(page).to_not have_content(animal_shelter.name)
+      expect(page).to_not have_button('Delete')
+
+    end
+  end
+end
