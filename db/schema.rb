ac4155aece581969bffa7d1a5b21ca9d59dd6116
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-19 21:32:34 -0600

    Adds seed file to fix when db gets out of sync

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200320000202) do
+ActiveRecord::Schema.define(version: 20200320031313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 20200320000202) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "sex"
     t.bigint "shelter_id"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+shelter_1 = Shelter.create!(name: "Dog-Haven",
+                           address:  "1234 Barkers Way",
+                           city: "Beagle",
+                           state: "MA",
+                           zip: "01001")
+
+shelter_2 = Shelter.create!(name: "The Cat Palace",
+                        address: "998 Kings Ct",
+                        city: "Kings Landing",
+                        state: "CA",
+                        zip: "90210")
+
+shelter_4 = Shelter.create!(name: "Joe Exotic's House",
+                     address: "7785 Jay St",
+                     city: "Lakewood",
+                     state: "CO",
+                     zip: "80232")
+
+
+pet_1 = Pet.create!(name: "Charlotte",
+                   age: 13,
+                   sex: "Female",
+                   shelter_id: 1)
+
+pet_2 = Pet.create!(name: "Sydney",
+                  age: 13,
+                  sex: "Female",
+                  shelter_id: 1)
+
+pet_3 = Pet.create!(name: "Josie",
+                 age: 4,
+                 sex: "Female",
+                 shelter_id: 2)
+
+pet_4 = Pet.create!(name: "Beef",
+                   age: 8,
+                   sex: "Male",
+                   shelter_id: 3)
