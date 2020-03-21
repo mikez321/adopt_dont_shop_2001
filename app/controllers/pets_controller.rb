4eb11d016e27dc134289bd56fefd5e8fb9d7cfaa
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-21 11:18:48 -0600

    Create action for pet update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,4 +13,21 @@ class PetsController < ApplicationController
     redirect_to "/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  private
+
+  def pet_params
+    params.permit(:image, :name, :description, :age, :sex)
+  end
+
 end
