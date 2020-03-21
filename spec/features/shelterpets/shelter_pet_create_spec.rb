90c4708b08fd19e68a225e0e58ad9d1faceb9d88
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-20 22:28:25 -0600

    Create shelter pet creation test

---
+++ spec/features/shelterpets/shelter_pet_create_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "when I visit the shelter page" do
+  describe "and I click a link for Create Pet" do
+    it "I am taken to a /new page and see a form to create a pet" do
+
+      shelter_1 = Shelter.create(name: "Dog-Haven",
+                                 address:  "1234 Barkers Way",
+                                 city: "Beagle",
+                                 state: "MA",
+                                 zip: "01001")
+
+      visit "/shelters/#{shelter_1.id}/"
+
+      click_link "Create Pet"
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/new")
+
+      fill_in :image, with: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/sydney.jpg"
+      fill_in :name, with: "Sydney"
+      fill_in :description, with: "I love to sleep, but from 3 till 4, I'm ready to hunt!  OWWWWWWWW!!!"
+      fill_in :age, with: 13
+      fill_in :sex, with: "Female"
+
+      click_button "Create Pet"
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
+
+      expect(page).to have_content("Sydney")
+      expect(page).to have_content("I love to sleep, but from 3 till 4, I'm ready to hunt!  OWWWWWWWW!!!")
+      
+  end
+end
