cdad625bfe79a42747ea4679d1ffc0cb8496a195
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-21 15:44:04 -0600

    Create global-ish links to main pages and tests

---
+++ spec/features/shelterpets/shelter_pets_view_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "when I visit a shelter show page", type: :feature do
+  it "I see a link to that shelter's pets page" do
+
+    shelter_1 = Shelter.create(name: "Dog-Haven",
+                               address:  "1234 Barkers Way",
+                               city: "Beagle",
+                               state: "MA",
+                               zip: "01001")
+
+    pet_1 = shelter_1.pets.create!(name: "Charlotte",
+                                   age: 13,
+                                   sex: "Female",
+                                   image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/charlotte.jpg")
+    shelter_2 = Shelter.create(name: "Bork Bork",
+                               address:  "9939 Fox St",
+                               city: "Lakewood",
+                               state: "CO",
+                               zip: "80232")
+
+    pet_2 = shelter_1.pets.create!(name: "Sydney",
+                                   age: 13,
+                                   sex: "Female",
+                                   image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/sydney.jpg")
+
+    visit "/shelters/#{shelter_1.id}"
+      expect(page).to have_content("See Our Pets")
+        click_link "See Our Pets"
+          expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
+
+    visit "/shelters/#{shelter_2.id}"
+      expect(page).to have_content("See Our Pets")
+        click_link "See Our Pets"
+          expect(current_path).to eq("/shelters/#{shelter_2.id}/pets")
+  end
+end
