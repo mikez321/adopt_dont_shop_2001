789f6fda9f416f8d26db501d8ff3e20cbc9b81fc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-22 14:38:46 -0600

    Reseed db and set fixed width of pet containers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,32 @@ Pet.destroy_all
 Shelter.destroy_all
 
 shelter_1 = Shelter.create!(name: "Dog-Haven",
-                           address:  "1234 Barkers Way",
-                           city: "Beagle",
-                           state: "MA",
-                           zip: "01001")
+                            address:  "1234 Barkers Way",
+                            city: "Beagle",
+                            state: "MA",
+                            zip: "01001")
 
 shelter_2 = Shelter.create!(name: "Little King Trashmouth's Animal Rescue",
-                        address: "998 Ocean Ave",
-                        city: "Ocean Town",
-                        state: "NY",
-                        zip: "10010")
+                            address: "998 Ocean Ave",
+                            city: "Ocean Town",
+                            state: "NY",
+                            zip: "10010")
 
 pet_1 = shelter_1.pets.create!(name: "Charlotte",
-                   age: 13,
-                   sex: "Female",
-                   image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/charlotte.jpg")
+                               description: "I like to sleep until it's time to eat.",
+                               age: 13,
+                               sex: "Female",
+                               image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/charlotte.jpg")
 
 
 pet_2 = shelter_1.pets.create!(name: "Sydney",
-                  age: 13,
-                  sex: "Female",
-                  image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/sydney.jpg")
+                               description: "I'm a snuggler and a lover until 3.  Then it's time to hunt!  OWWWWW!",
+                               age: 13,
+                               sex: "Female",
+                               image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/sydney.jpg")
 
 pet_3 = shelter_2.pets.create!(name: "Huckleberry",
-                 age: 8,
-                 sex: "Male",
-                 image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/hb.jpg")
+                               description: "I love adventure and haircuts!",
+                               age: 8,
+                               sex: "Male",
+                               image: "https://raw.githubusercontent.com/mikez321/adopt_dont_shop_2001/master/app/assets/images/hb.jpg")
